5e2c518a0c5eb6ae6b5458f34114d7331080d56f
Emily Hill <emhillmed2021@gmail.com>
2022-10-10 18:36:42 -0500

    started index.js

--- index.js
+++ index.js
@@ -1,3 +1,71 @@
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 const {writeFile, copyFile} = require(./utils/generate-site); 
+
+const promptEmployee = employeeData => {
+
+    return inquirer
+    .prompt([
+        {
+            type: 'text',
+            name: 'name',
+            message: 'What is the employee name?(Required)',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please type the employee's name.");
+                    return false; 
+                }
+            }
+        },
+        { 
+            type: 'text',
+            name: 'id',
+            message: "What is the employee's ID?(Required)",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log('Please type an ID.')
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: "What is the employee's email?"
+        },
+        {
+            type: 'text',
+            name: 'github',
+            message: "What is the engineer's Github username?"
+        },
+        {
+            type: 'text',
+            name: 'school',
+            message: "What is the intern's school name?"
+        },
+        {
+            type: 'text',
+            name: 'officeNumber',
+            message: "What is the manager's office number?"
+        }
+    ])
+}; 
+
+promptEmployee()
+.then(employeeData => {
+    return generatePage(employeeData);
+})
+.then(pageHTML => {
+    return writeFile(pageHTML);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse); 
+    return copyFile();
+})
+.then(copyFileResponse => {
+    console.log(copyFileResponse); 
+})
