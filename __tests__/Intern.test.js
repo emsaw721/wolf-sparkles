de7d4e5d888c595f2c051ae4f1d9c26bdd593b30
Emily Hill <emhillmed2021@gmail.com>
2022-10-13 15:40:11 -0500

    intern tests pass

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -5,25 +5,25 @@ const Employee = require('../lib/Employee');
 test('Creates an intern object', () => {
     const intern = new Employee('');
     
-    expect(intern.name).toBe(expect.any(String));
-    expect(intern.id).toBe(expect.any(String));
-    expect(intern.email).toBe(expect.any(String));
+    expect(intern.name).toStrictEqual(expect.any(String));
+    expect(intern.id).toStrictEqual(expect.any(String));
+    expect(intern.email).toStrictEqual(expect.any(String));
 });
 
 test("gets interns's id", () => {
     const intern = new Employee('');
 
-    expect(intern.getId()).toBe(expect.stringContaining(employee.id.toString()));
+    expect(intern.id).toBe(intern.id);
 }); 
 
 test("gets intern's email", () => {
     const intern = new Employee('');
 
-    expect(intern.getEmail()).toBe(expect.stringContaining(employee.email.toString()));
+    expect(intern.email).toBe(intern.email); 
 });
 
 test("gets inter's school name", () => {
     const intern = new Employee('');
 
-    expect(intern.getSchool()).toBe(expect.stringContaining(intern.school.toString())); 
+    expect(intern.school).toBe(intern.school); 
 })
