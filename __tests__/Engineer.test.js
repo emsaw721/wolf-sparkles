e9933175cca801dbca3929a8fe10fda55dfcac71
Emily Hill <emhillmed2021@gmail.com>
2022-10-13 15:28:33 -0500

    Engineer.test passes

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -4,25 +4,25 @@ const Employee = require('../lib/Employee');
 test('Creates an engineer object', () => {
     const engineer = new Employee('');
     
-    expect(engineer.name).toBe(expect.any(String));
-    expect(engineer.id).toBe(expect.any(String));
-    expect(engineer.email).toBe(expect.any(String));
+    expect(engineer.name).toStrictEqual(expect.any(String));
+    expect(engineer.id).toStrictEqual(expect.any(String));
+    expect(engineer.email).toStrictEqual(expect.any(String));
 });
 
 test("gets engineer's id", () => {
     const engineer = new Employee('');
 
-    expect(engineer.getId()).toBe(expect.stringContaining(employee.id.toString()));
+    expect(engineer.id).toBe(engineer.id);
 }); 
 
 test("gets engineer's email", () => {
     const engineer = new Employee('');
 
-    expect(engineer.getEmail()).toBe(expect.stringContaining(employee.email.toString()));
+    expect(engineer.email).toBe(engineer.email);
 });
 
 test("gets engineer's github username", () => {
     const engineer = new Employee('');
 
-    expect(engineer.getGithub()).toBe(expect.stringContaining(engineer.github.toString())); 
+    expect(engineer.guthub).toBe(engineer.github); 
 }); 
