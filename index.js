f63a1c12abc036e01f66552bdc91a88559b1ab7f
Emily Hill <emhillmed2021@gmail.com>
2022-10-13 23:59:19 -0500

    another try, still not working

--- index.js
+++ index.js
@@ -6,144 +6,17 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
-const internQuestions = [
-    {
-        type: 'text',
-        name: 'name',
-        message: 'What is the employee name?(Required)',
-        validate: nameInput => {
-            if(nameInput) {
-                return true;
-            } else {
-                console.log("Please type the employee's name.");
-                return false; 
-            }
-        }
-    },
-    { 
-        type: 'text',
-        name: 'id',
-        message: "What is the employee's ID?(Required)",
-        validate: nameInput => {
-            if(nameInput) {
-                return true;
-            } else {
-                console.log('Please type an ID.')
-                return false; 
-            }
-        }
-    },
-    {
-        type: 'text',
-        name: 'email',
-        message: "What is the employee's email?"
-    },
-    {
-        type: 'text',
-        name: 'school',
-        message: "What is the intern's school name?"
-    },
-]
+teamArr= [];
 
-const engineerQuestions = [
-    {
-        type: 'text',
-        name: 'name',
-        message: 'What is the employee name?(Required)',
-        validate: nameInput => {
-            if(nameInput) {
-                return true;
-            } else {
-                console.log("Please type the employee's name.");
-                return false; 
-            }
-        }
-    },
-    { 
-        type: 'text',
-        name: 'id',
-        message: "What is the employee's ID?(Required)",
-        validate: nameInput => {
-            if(nameInput) {
-                return true;
-            } else {
-                console.log('Please type an ID.')
-                return false; 
-            }
-        }
-    },    { 
-        type: 'text',
-        name: 'id',
-        message: "What is the employee's ID?(Required)",
-        validate: nameInput => {
-            if(nameInput) {
-                return true;
-            } else {
-                console.log('Please type an ID.')
-                return false; 
-            }
-        }
-    },
-    {
-        type: 'text',
-        name: 'email',
-        message: "What is the employee's email?"
-    },   
-    {
-        type: 'text',
-        name: 'github',
-        message: "What is the engineer's Github username?"
-    },
-]
-
-const managerQuestions = [
-    {
-        type: 'text',
-        name: 'name',
-        message: 'What is the employee name?(Required)',
-        validate: nameInput => {
-            if(nameInput) {
-                return true;
-            } else {
-                console.log("Please type the employee's name.");
-                return false; 
-            }
-        }
-    },
-    { 
-        type: 'text',
-        name: 'id',
-        message: "What is the employee's ID?(Required)",
-        validate: nameInput => {
-            if(nameInput) {
-                return true;
-            } else {
-                console.log('Please type an ID.')
-                return false; 
-            }
-        }
-    },
-    {
-        type: 'text',
-        name: 'email',
-        message: "What is the employee's email?"
-    },
-    {
-        type: 'text',
-        name: 'officeNumber',
-        message: "What is the manager's office number?"
-    }
-]
-
-const promptEmployee = employeeData => {
+function promptEmployee() {
     return inquirer
     .prompt([
-{
-    type: 'confirm',
-    name:'add',
-    message:'Would you like to add an employee?',
+// {
+//     type: 'confirm',
+//     name:'add',
+//     message:'Would you like to add an employee?',
  
-},
+// },
 {
     type: 'checkbox',
     name:'type',
@@ -151,13 +24,13 @@ const promptEmployee = employeeData => {
     choices:['Intern', 'Engineer', 'Manager'],
     when: (answers) => {
         if(answers.type === 'Intern') {
-            internQuestions; 
+            addIntern();
         }
         else if (answers.type === 'Engineer') {
-            engineerQuestions; 
+            addEngineer();
         }
         else{
-            managerQuestions; 
+            addManager(); 
         }
     }
 },
@@ -176,7 +49,7 @@ const promptEmployee = employeeData => {
         //convert into classes, into arr 
         //create team 
         console.log(employeeData)
-        // const pageTemplate= generatePage(employeeData);
+        // const pageTemplate= generatePage(teamArr);
         // fs.writeFile('./dist/index.html', pageTemplate, err => {
         //     if(err) {
         //        return console.log(err); 
@@ -188,7 +61,143 @@ const promptEmployee = employeeData => {
     .catch(err => {
         console.log(err)
     })
+}
 
+function addIntern()  {
+    inquirer.prompt(
+    [
+        {
+            type: 'text',
+            name: 'name',
+            message: 'What is the employee name?(Required)',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please type the employee's name.");
+                    return false; 
+                }
+            }
+        },
+        { 
+            type: 'text',
+            name: 'id',
+            message: "What is the employee's ID?(Required)",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log('Please type an ID.')
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: "What is the employee's email?"
+        },
+        {
+            type: 'text',
+            name: 'school',
+            message: "What is the intern's school name?"
+        },
+    ]).then(answers => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school); 
+        teamArr.push(intern); 
+        promptEmployee(); 
+    
+    })
+    }
+    
+    function addEngineer() {
+        inquirer.prompt([
+        {
+            type: 'text',
+            name: 'name',
+            message: 'What is the employee name?(Required)',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please type the employee's name.");
+                    return false; 
+                }
+            }
+        },
+        { 
+            type: 'text',
+            name: 'id',
+            message: "What is the employee's ID?(Required)",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log('Please type an ID.')
+                    return false; 
+                }
+            }
+        },    
+        {
+            type: 'text',
+            name: 'email',
+            message: "What is the employee's email?"
+        },   
+        {
+            type: 'text',
+            name: 'github',
+            message: "What is the engineer's Github username?"
+        },
+    ]).then(answers => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        teamArr.push(engineer);
+        promptEmployee();
+    })
+    }
+    
+    function addManager() {
+        inquirer.prompt([
+        {
+            type: 'text',
+            name: 'name',
+            message: 'What is the employee name?(Required)',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please type the employee's name.");
+                    return false; 
+                }
+            }
+        },
+        { 
+            type: 'text',
+            name: 'id',
+            message: "What is the employee's ID?(Required)",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log('Please type an ID.')
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: "What is the employee's email?"
+        },
+        {
+            type: 'text',
+            name: 'officeNumber',
+            message: "What is the manager's office number?"
+        }
+    ]).then(answers => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        teamArr.push(manager); 
+        promptEmployee(); 
+    })
     }
 
-promptEmployee();
+promptEmployee(); 
